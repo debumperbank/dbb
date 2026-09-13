17c4dcc652a9daf48f853b500fb0b341628a8253
Gomez <macbook@gomezs-mbp.home>
2026-09-13 09:20:01 +0200

    added vercel analytics

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { createAdminClient } from '@/lib/supabase/admin';
+import { Analytics } from "@vercel/analytics/next"
 
 async function getStats() {
   const supabase = createAdminClient();
